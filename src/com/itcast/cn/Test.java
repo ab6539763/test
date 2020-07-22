9dcaa9f7f00f37bc635f963d6b12146ebe234eb4
xiao <xiao@qq.com>
2020-07-22 16:40:18 +0800

    2

--- src/com/itcast/cn/Test.java
+++ src/com/itcast/cn/Test.java
@@ -9,5 +9,8 @@ public class Test {
         System.out.println("+++++++++++++++++");
         System.out.println("+++++++++++++++++");
         System.out.println("+++++++++++++++++");
+        System.out.println("+++++++++++++++++");
+        System.out.println("+++++++++++++++++");
+        System.out.println("+++++++++++++++++");
     }
 }
